52e086daf21af49e62e3a066224f1c0512a1cd60
ctorney <colin.j.torney@gmail.com>
2024-11-17 11:14:49 +0000

    clean up of tmux

--- .config/zsh/config.zsh
+++ .config/zsh/config.zsh
@@ -44,7 +44,7 @@ source $PLUGIN_PATH/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source $PLUGIN_PATH/zsh-ask/zsh-ask.zsh
 source $PLUGIN_PATH/zsh-ai-commands/zsh-ai-commands.zsh
 source $PLUGIN_PATH/k/k.sh
-source <(fzf --zsh)
+# source <(fzf --zsh)
 
 
 conda activate tf
@@ -52,18 +52,5 @@ conda activate tf
 [[ ! -f $HOME/.config/zsh/.p10k.zsh ]] || source $HOME/.config/zsh/.p10k.zsh
 
 
-# add a function to ssh and attach to tmux if it exists, or start a session if it doesn't
-function tsh {
-  ssh $1 -t "tmux -CC new-session -A -s main"
-}
-
-# check if we're in a tmux session
-if [ -n "$TMUX" ]; then
-  # if we are, set the terminal title to the session name
-  printf "\033k$TMUX\033\\"
-  tmux setenv -g MPLBACKEND $MPLBACKEND
-  tmux setenv -g MPLBACKEND_TRANSPARENT $MPLBACKEND_TRANSPARENT
-  tmux setenv -g ITERMPLOT_LINES $ITERMPLOT_LINES
-fi
 bindkey  "^[[H"   beginning-of-line
 bindkey  "^[[F"   end-of-line
