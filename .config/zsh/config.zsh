8db48f6a2eb4e5a9b6edc2cf8622f52a05850066
ctorney <colin.j.torney@gmail.com>
2025-01-09 09:06:59 +0000

    exec tmux

--- .config/zsh/config.zsh
+++ .config/zsh/config.zsh
@@ -10,12 +10,9 @@ export MPLBACKEND=module://matplotlib-backend-terminal
 export MPLBACKEND_TRANSPARENT=1
 export MPLBACKEND_CHAFA_OPTS="-f kitty --passthrough tmux"
 
-if [ -n "$SSH" ]; then
-  /usr/bin/ssh $SSH || printf "SSH connection failed\n"
-fi
 
 if [ -z "$TMUX" ]; then
-  tmux new-session -A -s TMUX
+  exec tmux new-session -A -s TMUX
 fi
 
 PLUGIN_PATH=$HOME/.config/zsh/plugins
@@ -54,27 +51,21 @@ source <(fzf --zsh)
 
 
 export FZF_DEFAULT_OPTS="--preview='[[ \$(file --mime {}) =~ binary ]] && echo {} is a binary file || (bat --style=numbers --color=always {} --style=header,grid || cat {}) 2> /dev/null | head -300' --preview-window=right:50%:wrap --bind '?:toggle-preview' --bind 'ctrl-y:become([[ -f {} && -r {} ]] && nvim {} || echo \"Not a readable text file\")'
---header 'Press CTRL-Y to edit the file'"
+--header ''"
 
 # Exclude those directories even if not listed in .gitignore, or if .gitignore is missing
 FD_OPTIONS="--follow --exclude .git --exclude node_modules"
 
-# Change behavior of fzf dialogue
-# export FZF_DEFAULT_OPTS="--multi --inline-info --preview='[[ \$(file --mime {}) =~ binary ]] && echo {} is a binary file || (bat --style=numbers --color=always {} || cat {}) 2> /dev/null | head -300' --preview-window='right:wrap' --bind='f3:execute(bat --style=numbers {} || less -f {}),f2:toggle-preview,ctrl-d:half-page-down,ctrl-u:half-page-up,ctrl-a:select-all+accept,ctrl-y:execute-silent(echo {+} | pbcopy)' --bind 'enter:become(nvim {})'"
-
-# Change find backend
 # Use 'git ls-files' when inside GIT repo, or fd otherwise
 export FZF_DEFAULT_COMMAND="git ls-files --cached --others --exclude-standard | fd --type f --type l $FD_OPTIONS"
 
 # Find commands for "Ctrl+T" and "Opt+C" shortcuts
 export FZF_CTRL_T_COMMAND="fd $FD_OPTIONS"
 export FZF_ALT_C_COMMAND="fd --type d $FD_OPTIONS"
-# CTRL-Y to copy the command into clipboard using pbcopy
 export FZF_CTRL_R_OPTS="
   --preview-window='right:wrap:hidden'
   --color header:italic
   --header ''"
-
 
 export FZF_ALT_C_OPTS="
   --preview='ls -la --color=always {} | head -200'
@@ -85,7 +76,6 @@ export FZF_ALT_C_OPTS="
 conda activate tf
 
 [[ ! -f $HOME/.config/zsh/.p10k.zsh ]] || source $HOME/.config/zsh/.p10k.zsh
-
 
 bindkey "^[[H"   beginning-of-line
 bindkey "^[[F"   end-of-line
